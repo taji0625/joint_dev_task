6677967c327c4341652ff7e9aed3c2211e562a6f
Takuma Tajikawa <takumatajikawa@TakumanoMacBook-Pro.local>
2020-06-06 07:42:27 +0900

    Q11の訂正

--- task.rb
+++ task.rb
@@ -96,9 +96,8 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports.flatten!.uniq!
-  puts "ユーザーの趣味一覧"
-  sports.each.with_index(1) do |hobby, i|
+  puts "ユーザーの趣味一覧" 
+  sports.flatten.uniq.each.with_index(1) do |hobby, i|
     puts "No#{i} #{hobby}"
   end
 end
